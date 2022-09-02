7e98cbc8f74cd0c56f9672422a3c8a44c054a731
Alex <agonzalez.alebrije@gmail.com>
2022-09-01 22:09:06 -0500

    AB-83 TypeScript Express 9.1

--- transactionsTypeScript/main.ts
+++ transactionsTypeScript/main.ts
@@ -7,13 +7,13 @@ const data = `id,currency,value
 
 const lines = data.split('\n').slice(1)
 
-type Transaction = {
+interface Transaction {
     id: string
     currency: string
     value: number
 }
 
-const transactions: Transaction[] = lines.map(line => {
+const transactions: Array<Transaction> = lines.map(line => {
     const [id, currency, valueString] = line.split(',')
     return { id, currency, value: Number(valueString) }
 })
